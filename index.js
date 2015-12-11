df0bb2d753ab70fb1a344ae4d8443b7e408d3942
Marck <mercercountynj@gmail.com>
2015-12-11 16:56:01 -0500

    more jquery

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ $(document).ready(function(){
     });
     
     
- 
+    $("button").click(function(){
+        $("#div1").fadeIn();
+        $("#div2").fadeIn("slow");
+        $("#div3").fadeIn(3000);
+    });
+    
+    $('img').fadeIn(5000);
     
     
     
