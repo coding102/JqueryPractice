600084718b5e404518b2e6e114cdb563959e1c57
Marck <mercercountynj@gmail.com>
2015-12-11 16:41:04 -0500

    jquery toggles added

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    
+    $("#nav").mousedown(function(){
+        alert("Call us Today!");
+    });
+    
+    $("#flip").click(function(){
+        $(".container2").slideToggle("slow");
+    });
+    
+    
+ 
+    
+    
+    
+});
